1bec7699dfa872af1ebd6f7e81085a237f3fc857
suvamshrestha <suvamshrestha93@gmail.com>
2025-05-14 17:01:37 +0545

    Add files via upload

---
+++ lessson 2 ; ACP/ACP.py
@@ -0,0 +1,8 @@
+DOB = "8/9/1995"
+print ("date of birth of ram is ",DOB)
+FOB ="9/12/2005"
+print ("date of birth of sita is",FOB)
+HOB = "10/12/2000"
+print ("date of birth of gita is",HOB)
+KOB = "11/12/2001"
+print ("date of birth of rita is",KOB)
