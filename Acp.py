5b00c109de46bd292e085c50eca5473b87788084
suvamshrestha <suvamshrestha93@gmail.com>
2025-05-17 10:50:28 +0545

    Add files via upload

---
+++ Acp.py
@@ -0,0 +1,8 @@
+text = str("congratulation")
+revText = text[::-1]
+text = revText
+print("Reversed text: ", text)
+print(text)
+word = input("enter the word in lowercase: ")
+uppercase_word = word.upper()
+print("Uppercase word: ", uppercase_word)
