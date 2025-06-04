13af69c07d9504d2da9709753696067246690f91
suvamshrestha <suvamshrestha93@gmail.com>
2025-06-04 16:42:05 +0545

    Add files via upload

---
+++ ininte.py
@@ -0,0 +1,3 @@
+i = 5
+while i <= 5:
+    print(" suvam")
---
+++ sumofnaturalnum.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+sum = 0 
+i = 1
+while i <= num:
+    sum = sum + i
+    i = i + 1
+    print("The sum of the first", num, "natural numbers is:", sum)
