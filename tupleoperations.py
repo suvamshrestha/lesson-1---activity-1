597ca7ec400a78afa6ad9c0ff343c12631e6919e
suvamshrestha <suvamshrestha93@gmail.com>
2025-06-30 16:48:02 +0545

    Add files via upload

---
+++ tupleoperations.py
@@ -0,0 +1,14 @@
+a = ("apple", 23, 2.5, True  )
+print(a)
+# creating a tuple
+b = ("banana", 45, 3.0, False)
+print(b)
+b = b + ("orange", 12, 1.5, True)
+print(b)
+c = (2,3,5,6,2,1,3)
+print(c.count(2))
+t = (1,2,3,4,5)
+_slice = t[2:4]
+print(_slice)
+_slice = t[:4]
+print(_slice)
